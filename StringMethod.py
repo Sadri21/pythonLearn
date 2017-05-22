cbb5021f1a6b5138c86cfec35d475eea8f2ba693
sadri21 <sadriashari21@gmail.com>
2017-05-22 22:37:41 +0700

    String Method

---
+++ StringMethod.py
@@ -0,0 +1,19 @@
+#accesStringByIndex
+fifth_latter = "PARTY"[4]
+print (fifth_latter)
+
+#string method
+parrot = "Nerwegian Blue"
+pi = 3.14
+print (len(parrot))
+print (parrot.lower())
+print (parrot.upper())
+print (str(pi))
+
+#string with % variable
+name = input("What Is Your Name?")
+car = input ("What is Yout Favorite Car?")
+string_1 = "Taman"
+string_2 = "Mobil"
+print ("sadri pergi ke %s. menggungakan %s." %(string_1, string_2))
+print ("my name is %s, im going to park with a %s" %(name, car))
