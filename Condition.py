fb3f075a901d5c7bab6b142f7c4de7b83e2d4267
sadri21 <sadriashari21@gmail.com>
2017-05-24 14:54:46 +0700

    Condition

---
+++ Condition.py
@@ -0,0 +1,27 @@
+# Set this to True if 17 < 328 or to False if it is not.
+bool_one = 17 < 328
+bool_one = True   # We did this one for you!
+
+# Set this to True if 100 == (2 * 50) or to False otherwise.
+bool_two = 100 == (2 * 50)
+bool_two = True
+
+# Set this to True if 19 <= 19 or to False if it is not.
+bool_three = 19 <= 19
+bool_three = True
+
+# Set this to True if -22 >= -18 or to False if it is not.
+bool_four = -22 >= -18
+bool_four =  False
+
+# Set this to True if 99 != (98 + 1) or to False otherwise.
+bool_five = 99 != (98 +1)
+bool_five = False
+
+#=======================================================================
+
+# 40 * 4 >= -4
+bool_four = 40 * 4 >= -4
+
+# 100 != 10**2
+bool_five = 100 != 10 **2
